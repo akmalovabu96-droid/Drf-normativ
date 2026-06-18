2669e1f340160048c7a2845151c640e04359bcfe
aakmalov-gamedev <aakmalov105@gmail.com>
2026-06-18 17:59:50 +0500

    drf-8 update: redis kutubxonasi va timezone + celerybeat scheduler

--- myproject/settings.py
+++ myproject/settings.py
@@ -140,10 +140,13 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-
 CELERY_ACCEPT_CONTENT = ['json']
-
 CELERY_TASK_SERIALIZER = 'json'
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TIMEZONE = 'Asia/Tashkent'
+CELERY_BEAT_SCHEDULER = (
+    'django_celery_beat.schedulers.DatabaseScheduler'
+)
 
 
 # Internationalization
